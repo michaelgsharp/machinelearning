3e9a49ab10f373d0d293ab44abe23fb11e57105d
Michael Sharp <misharp@microsoft.com>
2020-04-06 11:29:06 -0700

    testing update for forecastingpivot

--- test/Microsoft.ML.Tests/Transformers/ForecastingPivotFeaturizerTests.cs
+++ test/Microsoft.ML.Tests/Transformers/ForecastingPivotFeaturizerTests.cs
@@ -108,7 +108,7 @@ namespace Microsoft.ML.Tests.Transformers
         }
 
         [Fact]
-        public void Horizon2Test()
+        public void Horizon2IntegrationTest()
         {
             MLContext mlContext = new MLContext(1);
             var dataList = new[] {
@@ -132,6 +132,65 @@ namespace Microsoft.ML.Tests.Transformers
             var colA = debugView.ColumnView[1].Values;
             var pivotCol = debugView.ColumnView[3].Values;
             var horizonCol = debugView.ColumnView[4].Values;
+
+            // Correct output for:
+            // ColA,    ColA_Mean_MinWin1_MaxWin1,  Horizon
+            // 2.0,     1.0,                        1
+            // 3.0,     1.0,                        2
+            // 3.0,     2.0,                        1
+
+            Assert.True(pivotCol.Length == 3);
+
+            // Make sure the values are correct.
+            Assert.Equal(2.0, colA[0]);
+            Assert.Equal(3.0, colA[1]);
+            Assert.Equal(3.0, colA[2]);
+
+            Assert.Equal(1.0, pivotCol[0]);
+            Assert.Equal(1.0, pivotCol[1]);
+            Assert.Equal(2.0, pivotCol[2]);
+
+            Assert.Equal((UInt32)1, horizonCol[0]);
+            Assert.Equal((UInt32)2, horizonCol[1]);
+            Assert.Equal((UInt32)1, horizonCol[2]);
+
+            Done();
+        }
+        
+        private class Horizon2TestData
+        {
+            public double ColA { get; set; }
+            [VectorType(1,2)]
+            public double[] ColA_Mean_MinWin1_MaxWin1 { get; set; }
+        }
+
+        [Fact]
+        public void Horizon2Test()
+        {
+            MLContext mlContext = new MLContext(1);
+            var dataList = new[] {
+                new Horizon2TestData { ColA = 1.0, ColA_Mean_MinWin1_MaxWin1 = new [] { double.NaN, double.NaN } },
+                new Horizon2TestData { ColA = 2.0, ColA_Mean_MinWin1_MaxWin1 = new [] { double.NaN, 1.0 } },
+                new Horizon2TestData { ColA = 3.0, ColA_Mean_MinWin1_MaxWin1 = new [] { 1.0, 2.0 } }
+            };
+
+            //ColA_LagLead_Offsets_-1,1,2,3
+            //ColA_Lag_1
+            //ColA_Lead_1
+
+            var data = mlContext.Data.LoadFromEnumerable(dataList);
+
+            // Build the pipeline
+            var pipeline = mlContext.Transforms.PivotForecastingData(new string[] { "ColA_Mean_MinWin1_MaxWin1" });
+            var model = pipeline.Fit(data);
+            var output = model.Transform(data);
+            var schema = output.Schema;
+
+            //var index = 0;
+            var debugView = output.Preview();
+            var colA = debugView.ColumnView[0].Values;
+            var pivotCol = debugView.ColumnView[2].Values;
+            var horizonCol = debugView.ColumnView[3].Values;
 
             // Correct output for:
             // ColA,    ColA_Mean_MinWin1_MaxWin1,  Horizon
